7fdcb262e4965fc5d5236c0b186dc66cd2e7b2d2
sleepndraw <thepikahouse@gmail.com>
2024-07-17 14:05:04 +0200

    prueba cards

--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -19,7 +19,7 @@ Card.displayName = "Card"
 const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-2", className)}
+    className={cn("flex flex-col space-y-1.5 p-2 ", className)}
     {...props} />
 
 ))
