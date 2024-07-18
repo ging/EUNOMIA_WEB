6fca7d4a53a3ea382029a5a9c58ea59895563ca5
clara7227 <clarasalgado27@gmail.com>
2024-07-18 13:31:00 +0200

    responsive team ajustado

--- app/team/page.js
+++ app/team/page.js
@@ -95,7 +95,7 @@ export default function Team(props) {
       <main className="standard_margin ">
         <Heading level="h1">{t("team.title")}</Heading>
        
-        <section className="teammates flex flex-col gap-x-6 gap-y-10 md:grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+        <section className="teammates flex flex-col gap-x-6 gap-y-10 sm:grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
           {team["UPM Team"]?.members ? renderMembers(team["UPM Team"].members) : <p>No members found.</p>}
         </section>
       </main>
